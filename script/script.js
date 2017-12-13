cd4edf1686595f9a18acdeef88e02fa944b17afc
Cassul <aysa.garden@gmail.com>
2017-12-13 13:45:20 +1100

    draw added

--- script/script.js
+++ script/script.js
@@ -1,46 +1,69 @@
 const field = document.querySelectorAll('.col-sm');
 let counter = 0;
-let crossesCombinations = [];
-let knotsCombinations = [];
+let crossesMoves = [];
+let knotsMoves = [];
 const winningCombinations = [["1","2","3"],["4","5","6"],["7","8" , "9"], ["1", "5", "9"], ["3", "5", "7"], ["1", "4","7"], ["2", "5", "8"], ["3", "6", "9"]];
 let winner;
+let info;
+let knotsWin = 0;
+let crossesWin = 0;
+let numberOfMoves;
 
 field.forEach(function(element) {
 	element.addEventListener('click', todoClickHandler);
 });
 
 function todoClickHandler(event) {
-	counter++;
-	console.log('hey');
-
-	if (counter % 2 == 0) {
-		console.log('cross');
-		var cross = document.createElement('img');
-		cross.setAttribute('src', 'img/o.png');
-		event.target.appendChild(cross);
-		writeOnPanel("Player1 made move " + counter);
-		crossesCombinations.push(event.target.id);
-		checkCombination (crossesCombinations, winningCombinations);
-		if (winner=="Winner"){
-			alert('Winner knots');
-			cleanTheBoard();
+	if (event.target.childNodes.length == 1) {
+		counter++;
+		if (counter % 2 == 0) {
+			var cross = document.createElement('img');
+			cross.setAttribute('src', 'img/o.png');
+			event.target.appendChild(cross);
+			writeOnPanel("Player1 made move " + counter);
+			crossesMoves.push(event.target.id);
+			checkCombination (crossesMoves, winningCombinations);
+			if (winner=="Winner"){
+				knotsWin++;
+				alert('Winner knots');
+				cleanTheBoard();
+				totalWins();
+			}
+			if (knotsMoves.length + crossesMoves.length == 9 && winner!="Winner") {
+				alert('You both lost');
+				cleanTheBoard();
+				totalWins();
+			}
+		} else {
+			var knot = document.createElement('img');
+			knot.setAttribute('src', 'img/x.png');
+			event.target.appendChild(knot);
+			writeOnPanel("Player2 made move " + counter);
+			knotsMoves.push(event.target.id);
+			checkCombination (knotsMoves, winningCombinations);
+			if (winner=="Winner"){
+				crossesWin++;
+				alert('Winner crosses');
+				cleanTheBoard();
+				totalWins();
+			}
+			if (knotsMoves.length + crossesMoves.length == 9 && winner!="Winner") {
+				alert('You both lost');
+				cleanTheBoard();
+				totalWins();
+			}
 		}
 	} else {
-		console.log('knot');
-		var knot = document.createElement('img');
-		knot.setAttribute('src', 'img/x.png');
-		event.target.appendChild(knot);
-		writeOnPanel("Player1 made move " + counter);
-		knotsCombinations.push(event.target.id);
-		checkCombination (knotsCombinations, winningCombinations);
-		if (winner=="Winner"){
-			alert('Winner crosses');
-			cleanTheBoard();
-		}
+		alert('dont click cells twice, you idiot');
 	}
 }
 
 window.onload = writeOnPanel("Player 1 plays crosses" + "\n" + "Player2 plays knots");
+
+function totalWins () {
+	writeOnPanel("Crosses" + "\n" + "total wins: " + crossesWin + "\n" + "Knotes" + "\n" + "total wins: " + knotsWin);
+	}
+
 function writeOnPanel (text) {
 	let line = document.createElement('p');
 	let panel = document.querySelector('.panel');
@@ -57,8 +80,8 @@ function cleanTheBoard () {
 	while(text.length > 0) {
 		text[0].parentNode.removeChild(text[0]);
 	}
-	crossesCombinations = [];
-	knotsCombinations = [];
+	crossesMoves = [];
+	knotsMoves = [];
 	counter = 0;
 	winner = 0;
 }
@@ -77,4 +100,16 @@ function checkCombination (whatToCheck, winningCombinations) {
 			}
 		}
 	}
+}
+
+function hasImage(id) {
+    var childElements = document.getElementById(id).childNodes;
+    for (var i = 0; i < childElements.length; i++) {
+        if (childElements[i].localName != null && childElements[i].localName.toLowerCase() == "img") {
+            if (childElements[i].getAttribute('src') != null && childElements[i].getAttribute('src').toLowerCase() == "img.jpg") {
+                return true;
+            }
+        }
+    }
+    return false;
 }
