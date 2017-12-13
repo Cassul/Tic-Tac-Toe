290c48ec635669aff6b460f3769ff114ff35b9a4
Cassul <aysa.garden@gmail.com>
2017-12-13 11:41:34 +1100

    restart game added

--- script/script.js
+++ script/script.js
@@ -5,9 +5,16 @@ let knotsCombinations = [];
 const winningCombinations = [["1","2","3"],["4","5","6"],["7","8" , "9"], ["1", "5", "9"], ["3", "5", "7"], ["1", "4","7"], ["2", "5", "8"], ["3", "6", "9"]];
 let winner;
 
+field.forEach(function(element) {
+	element.addEventListener('click', todoClickHandler);
+});
+
 function todoClickHandler(event) {
 	counter++;
+	console.log('hey');
+
 	if (counter % 2 == 0) {
+		console.log('cross');
 		var cross = document.createElement('img');
 		cross.setAttribute('src', 'img/o.png');
 		event.target.appendChild(cross);
@@ -15,9 +22,11 @@ function todoClickHandler(event) {
 		crossesCombinations.push(event.target.id);
 		checkCombination (crossesCombinations, winningCombinations);
 		if (winner=="Winner"){
-			writeOnPanel('Winner knots');
+			alert('Winner knots');
+			cleanTheBoard();
 		}
 	} else {
+		console.log('knot');
 		var knot = document.createElement('img');
 		knot.setAttribute('src', 'img/x.png');
 		event.target.appendChild(knot);
@@ -25,7 +34,8 @@ function todoClickHandler(event) {
 		knotsCombinations.push(event.target.id);
 		checkCombination (knotsCombinations, winningCombinations);
 		if (winner=="Winner"){
-			writeOnPanel('Winner crosses');
+			alert('Winner crosses');
+			cleanTheBoard();
 		}
 	}
 }
@@ -38,20 +48,31 @@ function writeOnPanel (text) {
 	line.textContent = text;
 }
 
+function cleanTheBoard () {
+	let images = document.getElementsByTagName('img');
+	while(images.length > 0) {
+    images[0].parentNode.removeChild(images[0]);
+	}
+	let text = document.getElementsByTagName('p');
+	while(text.length > 0) {
+		text[0].parentNode.removeChild(text[0]);
+	}
+	crossesCombinations = [];
+	knotsCombinations = [];
+	counter = 0;
+	winner = 0;
+}
 
-field.forEach(function(element) {
-	element.addEventListener('click', todoClickHandler);
-});
 
 function checkCombination (whatToCheck, winningCombinations) {
 	
 	for (let i=0; i<winningCombinations.length; i++) {
-		let counter = 0;
+		let counter1 = 0;
 		for (let k=0; k<3; k++){
 			if(whatToCheck.indexOf(winningCombinations[i][k]) != -1) {
-				counter++;
+				counter1++;
 			}
-			if (counter == 3) {
+			if (counter1 == 3) {
 				winner = "Winner";
 			}
 		}
